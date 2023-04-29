7420938069d4b6180f9dddd90abec3ff5085535a
Md Amir Hossain <amirhossain.limon@gmail.com>
2023-04-29 21:57:37 +0600

    Added new route songdata/:id

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
-const MongoClient = require("mongodb").MongoClient;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -10,64 +10,6 @@ const port = process.env.PORT || 4001;
 // middleware
 app.use(cors());
 app.use(express.json());
-
-const client = new MongoClient(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-client.connect((err) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log("Connected to MongoDB");
-  }
-});
-
-const mainData = client.db("apidata").collection("main");
-const usersData = client.db("apidata").collection("users");
-
-app.post("/login", (req, res) => {
-  const { username, password } = req.body;
-
-  if (username === "my_username" && password === "my_secret_password") {
-    const token = jwt.sign({ username }, process.env.JWT_SECRET);
-    res.json({ token });
-  } else {
-    res.status(401).json({ message: "Incorrect username or password" });
-  }
-});
-
-app.get("/users", (req, res) => {
-  usersData.find().toArray((err, users) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.status(200).send(users);
-    }
-  });
-});
-
-app.post("/users", (req, res) => {
-  const user = req.body;
-  usersData.insertOne(user, (err, result) => {
-    if (err) {
-      res.status(400).send(err);
-    } else {
-      res.status(201).send(result.ops[0]);
-    }
-  });
-});
-
-app.get("/alldata", (req, res) => {
-  mainData.find().toArray((err, data) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.status(201).send(data);
-    }
-  });
-});
 
 const verifyJWT = (req, res, next) => {
   const token =
@@ -85,6 +27,72 @@ const verifyJWT = (req, res, next) => {
     res.status(401).json({ message: "Missing token" });
   }
 };
+
+const client = new MongoClient(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  try {
+    await client.connect((err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log("Connected to MongoDB");
+      }
+    });
+
+    const mainData = client.db("apidata").collection("main");
+    const usersData = client.db("apidata").collection("users");
+
+    app.post("/login", async (req, res) => {
+      const { username, password } = req.body;
+
+      if (username === "my_username" && password === "my_secret_password") {
+        const token = jwt.sign({ username }, process.env.JWT_SECRET);
+        res.send({ token });
+      } else {
+        res.status(401).json({ message: "Incorrect username or password" });
+      }
+    });
+
+    app.get("/users", async (req, res) => {
+      const query = {};
+      const cursor = usersData.find(query);
+      const users = await cursor.toArray();
+
+      res.send(users);
+    });
+
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+
+      const result = await usersData.insertOne(newUser);
+      res.send(result);
+    });
+
+    app.get("/alldata", async (req, res) => {
+      const query = {};
+      const allData = await mainData.find(query).toArray();
+
+      await res.status(200).json({ status: "success", data: allData });
+    });
+
+    app.get("/songdata/:id", async (req, res) => {
+      const songData = await mainData.findOne({
+        _id: new ObjectId(req.params.id),
+      });
+
+      await res.status(200).json({ status: "success", data: songData });
+    });
+  } finally {
+    console.log("Request completed");
+  }
+};
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Lyrics API");
