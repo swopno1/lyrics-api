3d182bf88fad29e5e836168a00cfc67d6d097ab2
Md Amir Hossain <amirhossain.limon@gmail.com>
2023-05-09 19:59:23 +0600

    Complete GET, POST, PUT, DELETE route for songs.

--- routes/songs.route.js
+++ routes/songs.route.js
@@ -7,15 +7,17 @@ const getDbCollection = require("../utils/db");
 // GET /songs - Retrieve all songs
 router.get("/", async (req, res) => {
   const query = {};
-  const songs = await getDbCollection("songs").find(query).toArray();
+  const songCollection = await getDbCollection("songs");
+  const songs = await songCollection.find(query).toArray();
 
   await res.status(200).send(songs);
 });
 
 // GET /songs/:id - Retrieve a specific song by ID
 router.get("/:id", async (req, res) => {
-  const song = await getDbCollection("songs").findOne({
-    _id: ObjectId(req.params.id),
+  const songCollection = await getDbCollection("songs");
+  const song = await songCollection.findOne({
+    _id: new ObjectId(req.params.id),
   });
   if (!song) {
     return res.status(404).send("Song not found");
@@ -29,8 +31,42 @@ router.post("/", async (req, res) => {
   if (!newSong.title || !newSong.lyrics) {
     return res.status(400).send("Title and artist are required");
   }
-  const result = await getDbCollection("songs").insertOne(newSong);
-  res.send(result.ops[0]);
+  const songCollection = await getDbCollection("songs");
+  const result = await songCollection.insertOne(newSong);
+
+  if (result.acknowledged && result.insertedId) {
+    // const insertedSong = await songsCollection.findOne({_id: result.insertedId});
+    res.status(201).send(newSong);
+  } else {
+    res
+      .status(500)
+      .send("An error occurred while creating a new song in the database");
+  }
+});
+
+// PUT /songs/:id - Update a specific song by ID
+router.put("/:id", async (req, res) => {
+  const songCollection = await getDbCollection("songs");
+  const query = { _id: new ObjectId(req.params.id) };
+  const updates = { $set: req.body };
+  const updatedSong = await songCollection.updateOne(query, updates, {
+    upsert: true,
+  });
+  if (!updatedSong) {
+    return res.status(404).send("Song not found");
+  }
+  res.status(200).send(updatedSong);
+});
+
+// DELETE /songs/:id - Delete a specific song by ID
+router.delete("/:id", async (req, res) => {
+  const songCollection = await getDbCollection("songs");
+  const query = { _id: new ObjectId(req.params.id) };
+  const result = await songCollection.deleteOne(query);
+  if (!result) {
+    return res.status(404).send("Song not found");
+  }
+  res.status(200).send(result);
 });
 
 module.exports = router;
