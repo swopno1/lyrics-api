3d7e0e89cbff4ec9a9d2aa47ffea249e678c11bb
Md Amir Hossain <amirhossain.limon@gmail.com>
2023-05-09 21:58:45 +0600

    Added swagger for documentation

--- routes/root.route.js
+++ routes/root.route.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     summary: Get a list of all songs
+ *     description: Returns a list of all songs in the database
+ *     responses:
+ *       200:
+ *         description: A list of songs
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Song'
+ */
 router.get("/", (req, res) => {
   res.send("Lyrics API");
 });
--- routes/songs.route.js
+++ routes/songs.route.js
@@ -5,6 +5,22 @@ const { ObjectId } = require("mongodb");
 const getDbCollection = require("../utils/db");
 
 // GET /songs - Retrieve all songs
+/**
+ * @swagger
+ * /songs:
+ *   get:
+ *     summary: Get a list of all songs
+ *     description: Returns a list of all songs in the database
+ *     responses:
+ *       200:
+ *         description: A list of songs
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Song'
+ */
 router.get("/", async (req, res) => {
   const query = {};
   const songCollection = await getDbCollection("songs");
